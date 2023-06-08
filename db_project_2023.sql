21fcd4242df5764ced1a9abe789061f951a36d83
Anastasia Manola <an.manola03@gmail.com>
2023-06-08 13:24:44 +0300

    Added comments

--- db_project_2023.sql
+++ db_project_2023.sql
@@ -14,10 +14,10 @@ CREATE TABLE IF NOT EXISTS Coaches (
 
 CREATE TABLE IF NOT EXISTS Teams (
     team_id SERIAL PRIMARY KEY,
-    teams_name VARCHAR(255) NOT NULL UNIQUE,
+    teams_name VARCHAR(255) NOT NULL UNIQUE,    --Χρήση keyword unique, για την εξασφάλιση της μοναδικότητας του ονόματος της ομάδας 
     home_field_name VARCHAR(255) NOT NULL,
     history_description TEXT,
-    home_wins INTEGER NOT NULL CHECK (home_wins >= 0) DEFAULT 0,
+    home_wins INTEGER NOT NULL CHECK (home_wins >= 0) DEFAULT 0,    --Χρήση check για την εξασφάλιση μη αρνητικού insert στα πεδία των στατιστικών της ομάδας
     away_wins INTEGER NOT NULL CHECK (away_wins >= 0) DEFAULT 0,
     home_loses INTEGER NOT NULL CHECK (home_loses >= 0) DEFAULT 0,
     away_loses INTEGER NOT NULL CHECK (away_loses >= 0) DEFAULT 0,
@@ -29,19 +29,20 @@ CREATE TABLE IF NOT EXISTS Games (
     game_id SERIAL PRIMARY KEY,
     home_team_id INTEGER NOT NULL REFERENCES Teams(team_id),
     away_team_id INTEGER NOT NULL REFERENCES Teams(team_id),
-    home_teams_score INTEGER NOT NULL CHECK (home_teams_score >= 0) DEFAULT 0,
+    home_teams_score INTEGER NOT NULL CHECK (home_teams_score >= 0) DEFAULT 0,  --Χρήση check για την εξασφάλιση μη αρνητικού insert στα πεδία του σκορ
     away_teams_score INTEGER NOT NULL CHECK (away_teams_score >= 0) DEFAULT 0,
     games_date DATE NOT NULL,
-    duration TIME NOT NULL DEFAULT 00:00:00
+    duration TIME NOT NULL DEFAULT 00:00:00       
 );
 
-ALTER TABLE Games ADD CONSTRAINT unique_game_per_day UNIQUE (home_team_id, away_team_id, games_date);
+--Εξασφάλιση μοναδικότητας αγώνα μεταξύ δύο ομάδων σε μία συγκεκριμένη ημέρα 
+ALTER TABLE Games ADD CONSTRAINT unique_game_per_day UNIQUE (home_team_id, away_team_id, games_date);   
 
 CREATE TABLE IF NOT EXISTS PlayerGameStatistics (
     player_id INTEGER NOT NULL REFERENCES Players(player_id) ON DELETE CASCADE,
     team_id INTEGER NOT NULL REFERENCES Teams(team_id),
     game_id INTEGER NOT NULL REFERENCES Games(game_id) ON DELETE CASCADE,
-    red_cards INTEGER NOT NULL CHECK (red_cards >= 0) DEFAULT 0,
+    red_cards INTEGER NOT NULL CHECK (red_cards >= 0) DEFAULT 0,             --Χρήση check για την εξασφάλιση μη αρνητικού insert στα πεδία των στατιστικών του παίκτη 
     yellow_cards INTEGER NOT NULL CHECK (yellow_cards >= 0) DEFAULT 0,
     goals INTEGER NOT NULL CHECK (goals >= 0) DEFAULT 0,
     canceled_goals INTEGER NOT NULL CHECK (canceled_goals >= 0) DEFAULT 0,
@@ -55,7 +56,7 @@ CREATE TABLE IF NOT EXISTS GameEvents (
     game_id INTEGER NOT NULL PRIMARY KEY REFERENCES Games(game_id) ON DELETE CASCADE,
     player_id INTEGER NOT NULL REFERENCES Players(player_id),
     moment INTEGER NOT NULL,
-    event_type VARCHAR(255) NOT NULL CHECK (UPPER(event_type) IN (UPPER('γκολ'), UPPER('ακυρωμένο γκολ'), UPPER('κόκκινη κάρτα'), UPPER('κίτρινη κάρτα'), UPPER('πέναλτι'), UPPER('κόρνερ')))
+    event_type VARCHAR(255) NOT NULL CHECK (UPPER(event_type) IN (UPPER('γκολ'), UPPER('ακυρωμένο γκολ'), UPPER('κόκκινη κάρτα'), UPPER('κίτρινη κάρτα'), UPPER('πέναλτι'), UPPER('κόρνερ'))) -- Χρήση check για την αποδοχή συγκεκριμένων συμβάντων του αγώνα (όπως περιγράφονται στην εκφώνηση)
 );
 
 CREATE TABLE IF NOT EXISTS HasPlayed (
