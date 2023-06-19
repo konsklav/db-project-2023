b2ecf09f2e50462f562ab4a6d37984c3b696f4ac
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2023-06-19 18:36:56 +0300

    Some modifications

--- db_project_2023.sql
+++ db_project_2023.sql
@@ -19,8 +19,8 @@ CREATE TABLE IF NOT EXISTS Teams (
     history_description TEXT,
     home_wins INTEGER NOT NULL CHECK (home_wins >= 0) DEFAULT 0,    --Χρήση check για την εξασφάλιση μη αρνητικού insert στα πεδία των στατιστικών της ομάδας
     away_wins INTEGER NOT NULL CHECK (away_wins >= 0) DEFAULT 0,
-    home_loses INTEGER NOT NULL CHECK (home_loses >= 0) DEFAULT 0,
-    away_loses INTEGER NOT NULL CHECK (away_loses >= 0) DEFAULT 0,
+    home_losses INTEGER NOT NULL CHECK (home_loses >= 0) DEFAULT 0,
+    away_losses INTEGER NOT NULL CHECK (away_loses >= 0) DEFAULT 0,
     home_draws INTEGER NOT NULL CHECK (home_draws >= 0) DEFAULT 0,
     away_draws INTEGER NOT NULL CHECK (away_draws >= 0) DEFAULT 0
 );
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS Games (
     home_teams_score INTEGER NOT NULL CHECK (home_teams_score >= 0) DEFAULT 0,  --Χρήση check για την εξασφάλιση μη αρνητικού insert στα πεδία του σκορ
     away_teams_score INTEGER NOT NULL CHECK (away_teams_score >= 0) DEFAULT 0,
     games_date DATE NOT NULL,
-    duration TIME NOT NULL DEFAULT 00:00:00       
+    duration TIME NOT NULL DEFAULT 00:00:00      
 );
 
 --Εξασφάλιση μοναδικότητας αγώνα μεταξύ δύο ομάδων σε μία συγκεκριμένη ημέρα 
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS GameEvents (
     game_id INTEGER NOT NULL PRIMARY KEY REFERENCES Games(game_id) ON DELETE CASCADE,
     player_id INTEGER NOT NULL REFERENCES Players(player_id),
     moment INTEGER NOT NULL,
-    event_type VARCHAR(255) NOT NULL CHECK (UPPER(event_type) IN (UPPER('γκολ'), UPPER('ακυρωμένο γκολ'), UPPER('κόκκινη κάρτα'), UPPER('κίτρινη κάρτα'), UPPER('πέναλτι'), UPPER('κόρνερ'))) -- Χρήση check για την αποδοχή συγκεκριμένων συμβάντων του αγώνα (όπως περιγράφονται στην εκφώνηση)
+    event_type VARCHAR(255) NOT NULL CHECK (UPPER(event_type) IN ('ΓΚΟΛ', 'ΑΚΥΡΩΜΕΝΟ ΓΚΟΛ', 'ΚΟΚΚΙΝΗ ΚΑΡΤΑ', 'ΚΙΤΡΙΝΗ ΚΑΡΤΑ', 'ΠΕΝΑΛΤΙ', 'ΚΟΡΝΕΡ')) -- Χρήση check για την αποδοχή συγκεκριμένων συμβάντων του αγώνα (όπως περιγράφονται στην εκφώνηση)
 );
 
 CREATE TABLE IF NOT EXISTS HasPlayed (
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS HasPlayed (
 );
 
 CREATE TABLE IF NOT EXISTS HasCoached (
-    coach_id INTEGER NOT NULL REFERENCES Games(game_id) ON DELETE CASCADE,
+    coach_id INTEGER NOT NULL REFERENCES Coaches(coach_id) ON DELETE CASCADE,
     team_id INTEGER NOT NULL REFERENCES Teams(team_id) ON DELETE CASCADE,
     in_transfer_date DATE NOT NULL,
     out_transfer_date DATE,
